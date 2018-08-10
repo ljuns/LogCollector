dc405948d7832bd84a6895ddc84775f428f4081b
ljuns <ljuns.dev@gmail.com>
2018-08-10 17:58:02 +0800

    完善功能

--- logcollector/src/main/java/cn/ljuns/logcollector/LogCollector.java
+++ logcollector/src/main/java/cn/ljuns/logcollector/LogCollector.java
@@ -1,39 +1,46 @@
 package cn.ljuns.logcollector;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringDef;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class LogCollector {
 
-    public static final String VERBOSE = "-v";
-    public static final String DEBUG = "-d";
-    public static final String INFO = "-i";
-    public static final String WARN = "-w";
-    public static final String ERROR = "-e";
-    public static final String ASSERT = "-a";
+    public static final String VERBOSE = "V/";
+    public static final String DEBUG = "D/";
+    public static final String INFO = "I/";
+    public static final String WARN = "W/";
+    public static final String ERROR = "E/";
+    public static final String ASSERT = "A/";
+
+    @StringDef({VERBOSE, DEBUG, INFO, WARN, ERROR, ASSERT})
+    @interface LogType {}
 
     private static final String DEFAULT_FORMAT = "yyyyMMdd_HHmmss_SSS";
 
-    @StringDef({VERBOSE, DEBUG, INFO, WARN, ERROR, ASSERT})
-    public @interface LogType{}
+    private File mCacheFile;    //缓存文件
+    private String mLogType;    //过滤类型
+    private String mBgColor;    //背景颜色
+    private String[] mLogcatColors; //默认的颜色集合
+    private boolean mCleanCache = false;    //是否清除缓存日志文件
+    private boolean mHasColors = true;  //是否设置颜色
 
-    private File mCacheFile;
-    private String mLogType;
-    private boolean mCleanCache = false;
-
-    private LogCollector(){}
+    private LogCollector() {
+        mLogcatColors = new String[]{"#ED008C", "#00FFOO", "#00FFFF", "#FFFFOO", "#FF00OO", "#00AEEF", "#FDC68C"};
+        mBgColor = "#000000";
+    }
 
     public static LogCollector getInstance() {
         return new LogCollector();
@@ -41,6 +48,7 @@ public class LogCollector {
 
     /**
      * 设置缓存文件
+     *
      * @param file
      * @return
      */
@@ -56,6 +64,7 @@ public class LogCollector {
 
     /**
      * 设置缓存类型
+     *
      * @param type 缓存类型
      * @return
      */
@@ -66,6 +75,7 @@ public class LogCollector {
 
     /**
      * 是否清除之前的缓存
+     *
      * @param cleanCache 是否清除
      * @return
      */
@@ -74,12 +84,127 @@ public class LogCollector {
         return this;
     }
 
-    @SuppressLint("SimpleDateFormat")
+    public LogCollector setHasColors(boolean hasColors) {
+        this.mHasColors = hasColors;
+        return this;
+    }
+
+    public LogCollector setBgColor(String bgColor) {
+        this.mBgColor = bgColor;
+        return this;
+    }
+
+    public LogCollector setColors(String verboseColor) {
+
+        return this;
+    }
+
+    public LogCollector setBackgroundColor() {
+
+        return this;
+    }
+
     public void start(Context context) {
+        createCacheFile(context);
+        new Thread(mLogRunnable).start();
+    }
+
+    private Runnable mLogRunnable = new Runnable() {
+        @Override
+        public void run() {
+            BufferedReader reader = null;
+            BufferedWriter writer = null;
+            try {
+                // 获取 logcat
+                String[] logcat = new String[]{"logcat", "-v", "time"};
+                Process process = Runtime.getRuntime().exec(logcat);
+
+                reader = new BufferedReader(
+                        new InputStreamReader(process.getInputStream(), "UTF-8"));
+                writer = new BufferedWriter(
+                        new OutputStreamWriter(new FileOutputStream(mCacheFile), "UTF-8"));
+
+                if (!mHasColors) {
+                    for (int i = 0; i < mLogcatColors.length; i++) {
+                        mLogcatColors[i] = "";
+                    }
+                    mBgColor = "#FFFFFF";
+                }
+
+                String str = null;
+                writer.write(" <body bgcolor=\" " + mBgColor + " \">");
+                while (((str = reader.readLine()) != null)) {
+                    Runtime.getRuntime().exec(new String[]{"logcat", "-c"});
+                    outputLogcat(writer, str);
+                }
+                writer.write("</body>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                CloseUtils.close(reader);
+                CloseUtils.close(writer);
+            }
+        }
+    };
+
+    /**
+     * 输出 logcat
+     * @param writer
+     * @param str
+     * @throws IOException
+     */
+    private void outputLogcat(BufferedWriter writer, String str) throws IOException {
+        if (mLogType != null) {
+            if (str.contains(mLogType)) {
+                write(writer, mLogcatColors[1], str);
+            }
+        } else {
+            if (str.contains(VERBOSE)) {
+                write(writer, mLogcatColors[0], str);
+            } else if (str.contains(DEBUG)) {
+                write(writer, mLogcatColors[1], str);
+            } else if (str.contains(INFO)) {
+                write(writer, mLogcatColors[2], str);
+            } else if (str.contains(WARN)) {
+                write(writer, mLogcatColors[3], str);
+            } else if (str.contains(ERROR)) {
+                write(writer, mLogcatColors[4], str);
+            } else if (str.contains(ASSERT)) {
+                write(writer, mLogcatColors[5], str);
+            } else {
+                write(writer, mLogcatColors[6], str);
+            }
+        }
+    }
+
+    /**
+     * 写数据
+     * @param writer
+     * @param color
+     * @param str
+     * @throws IOException
+     */
+    private void write(BufferedWriter writer, String color, String str) throws IOException {
+        writer.write("<font size=\"3\" color=\"" + color + "\">" + str + "</font></br>");
+        writer.newLine();
+    }
+
+    /**
+     * 创建缓存文件
+     * @param context
+     */
+    private void createCacheFile(Context context) {
         // 缓存文件
         DateFormat format = new SimpleDateFormat(DEFAULT_FORMAT);
-        String fileName = format.format(new Date(System.currentTimeMillis())) + ".txt";
-        String path = context.getApplicationContext().getExternalCacheDir() + "/log";
+        String fileName = format.format(new Date(System.currentTimeMillis())) + ".html";
+
+        String path = null;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                || !Environment.isExternalStorageRemovable()) {
+            path = context.getApplicationContext().getExternalCacheDir() + "/log";
+        } else {
+            path = context.getApplicationContext().getCacheDir() + "/log";
+        }
 
         File folder = new File(path);
         if (!folder.exists()) {
@@ -88,30 +213,24 @@ public class LogCollector {
 
         // 计算缓存日志大小
         computeSize(folder);
-
-        // 是否删除缓存日志
-        if (mCleanCache) {
-            cleanCache(folder);
-        }
-
-        // 初始化缓存文件
+        // 初始化
         initCacheFile(fileName, folder);
-
-        if (mLogType == null) {
-            mLogType = "-v";
-        }
-
-        new Thread(mLogRunnable).start();
     }
 
     /**
      * 初始化缓存文件
+     *
      * @param fileName 缓存文件名
-     * @param folder 文件夹
+     * @param folder   文件夹
      */
     private void initCacheFile(String fileName, File folder) {
+        // 是否删除缓存日志文件
+        if (mCleanCache) {
+            cleanCache(folder);
+        }
+
         if (mCacheFile == null) {
-            mCacheFile = new File(folder,  fileName);
+            mCacheFile = new File(folder, fileName);
             try {
                 mCacheFile.createNewFile();
             } catch (IOException e) {
@@ -126,6 +245,7 @@ public class LogCollector {
 
     /**
      * 删除缓存文件
+     *
      * @param folder 文件夹
      */
     private void cleanCache(File folder) {
@@ -136,6 +256,7 @@ public class LogCollector {
 
     /**
      * 获取缓存大小
+     *
      * @param folder log 文件夹
      */
     private void computeSize(File folder) {
@@ -151,41 +272,4 @@ public class LogCollector {
             cleanCache(folder);
         }
     }
-
-    private Runnable mLogRunnable = new Runnable() {
-        @Override
-        public void run() {
-            FileOutputStream os = null;
-            try {
-                List<String> list = new ArrayList<>();
-                list.add("logcat");
-                list.add(mLogType);
-                if (VERBOSE.equals(mLogType)) {
-                    list.add("time");
-                }
-
-                Process process = Runtime.getRuntime().exec(list.toArray(new String[list.size()]));
-                InputStream in = process.getInputStream();
-
-                os = new FileOutputStream(mCacheFile);
-                int len;
-                byte[] buf = new byte[1024];
-                while (((len = in.read(buf)) != -1)) {
-                    os.write(buf, 0, len);
-                    os.flush();
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (os != null) {
-                        os.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    };
 }
