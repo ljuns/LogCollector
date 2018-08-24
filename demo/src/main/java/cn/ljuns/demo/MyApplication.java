d2cc30470be143a17878e37f9208b5608a31cf37
ljuns <ljunsw@gmail.com>
2018-08-24 23:27:38 +0800

    修复bug

--- demo/src/main/java/cn/ljuns/demo/MyApplication.java
+++ demo/src/main/java/cn/ljuns/demo/MyApplication.java
@@ -3,6 +3,7 @@ package cn.ljuns.demo;
 import android.app.Application;
 
 import cn.ljuns.logcollector.LogCollector;
+import cn.ljuns.logcollector.util.LevelUtils;
 
 /**
  * Created by ljuns on 2018/8/18
@@ -13,15 +14,11 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-//        Map<String, String> map = new HashMap<>();
-//        map.put("MainActivity", )
-
         LogCollector.getInstance(this)
                 .setCleanCache(true)
                 .setTag("MainActivity")
-//                .setLevel(LevelUtils.D)
-//                .setLogcatColors(R.color.colorAccent, R.color.colorPrimaryDark, Color.RED, Color.GREEN)
+                .setLevel(LevelUtils.W)
+                .setTagWithLevel("EGL_emulation", LevelUtils.D)
                 .start();
     }
 }
